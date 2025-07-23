2f0017558ea8496b2a0084052abefdc3f6cc2a4a
SebRiez <Schreibtisch@aufschnitt.tv>
2025-07-23 08:22:37 +0100

    Update marker_code_generator_streamlit.py
    format for image text

--- marker_code_generator_streamlit.py
+++ marker_code_generator_streamlit.py
@@ -23,11 +23,13 @@ img2 = img2.resize((int(img2.width * scale), int(img2.height * scale)))
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image(img1, caption="before")
+    st.image(img1)
+    st.markdown("<div style='text-align:center; font-size:18px; font-weight:bold;'>Before</div>", unsafe_allow_html=True)
 
 with col2:
-    st.image(img2, caption="after")
-
+    st.image(img2)
+    st.markdown("<div style='text-align:center; font-size:18px; font-weight:bold;'>After</div>", unsafe_allow_html=True)
+    
 # File Upload
 uploaded_file = st.file_uploader("📁 Upload tab-delimited marker file (.txt)", type=["txt"])
 
